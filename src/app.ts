57a31a08766508e210b07ae5236c9d25af1a0646
WesllenWorld <wesllen.2020111052@unicap.br>
2024-08-22 22:56:41 -0300

    cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
 import express, { json } from "express"
 import router from "./routes/routes"
+import cors from 'cors'
 
 function createApp() {
     const app = express()
 
     app.use(json())
     app.use('/api', router)
+    app.use(cors())
+
+    // Add your own middleware here
+    const corsOptions = {
+        origin: ['http://sitedaora.com', 'http://pongkrell.com'],
+        methods: ['GET', 'POST'],
+    }
 
     return app
 }
